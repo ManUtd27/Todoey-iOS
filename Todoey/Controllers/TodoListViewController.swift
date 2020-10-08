cd5a4fb5324e66b9c9fa4c052914b67451b59641
Shawn Williams <jr.william09@gmail.com>
2020-10-07 19:15:25 -0500

    use a paren textField to hold the local textfield value then add item to array then reload the tableview to display the new Data items

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Find Mike", "Buy Eggos", "This is the way"]
+    var itemArray = ["Find Mike", "Buy Eggos", "This is the way"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,11 +65,19 @@ class TodoListViewController: UITableViewController {
     /// Displays an action sheet for the user to add a new item
     /// - Parameter sender: The user action from the view
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .actionSheet)
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // What will happen once the user clicks the add item button on our UIAlert
-            print("Success")
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
         }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
